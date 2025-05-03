61c041ef10a7c9f9d21d66c677e97894a22b5826
Joey <joey.kheireddine@outlook.com>
2025-05-03 23:17:08 +0200

    upd: cleanup

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,3 +1,26 @@
-import redis from '@autofun/redis';
+import Fastify from "fastify";
+import redis from "@autofun/redis";
+import logger from '@autofun/logger';
 
-console.log(redis);
+const fastify = Fastify({
+  logger
+});
+
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+const port = 3001;
+
+const start = async () => {
+  try {
+    await fastify.listen({ port });
+
+    const a = await redis.get("a");
+    fastify.log.info(`Server listening on port: ${port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -3,7 +3,7 @@ import logger from "@autofun/logger";
 import IORedis from "ioredis";
 
 dotenv.config({
-  path: '../../.env'
+  path: "../../.env",
 });
 
 const port = process.env.REDIS_PORT;
@@ -12,18 +12,24 @@ const username = process.env.REDIS_USERNAME;
 const password = process.env.REDIS_PASSWORD;
 const db = process.env.REDIS_DB;
 
-if (!port || !host || !username || !password) {
-  logger.error("Missing REDIS_* environment variables");
-  process.exit(1);
+if (process.env.NODE_ENV === "production") {
+  if (!port || !host || !username || !password) {
+    logger.error("Missing REDIS_* environment variables");
+    process.exit(1);
+  }
 }
 
-const redis = new IORedis({
-  port: Number(port),
-  host: String(host),
-  username: String(username),
-  password: String(password),
-  db: db ? Number(db) : 0,
-});
+const redis = new IORedis(
+  process.env.NODE_ENV === "production"
+    ? {
+        port: Number(port),
+        host: String(host),
+        username: String(username),
+        password: String(password),
+        db: db ? Number(db) : 0,
+      }
+    : {}
+);
 
 redis.on("connecting", () => {
   logger.info("Connecting to Redis");
