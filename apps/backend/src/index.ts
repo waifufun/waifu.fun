b61233e45f43d56ed27dc3ea33b47faaac002f36
Rayan Meziani <rayanmeziani95@outlook.com>
2025-05-13 11:06:34 +0200

    remove test route

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -21,16 +21,6 @@ fastify.get("/", (_, reply) => {
 	reply.send({ hello: "world" });
 });
 
-fastify.get("/check-balance", async (_, reply) => {
-	try {
-		const balance = await checkWalletBalance("0x73f7b1184b5cd361cc0f7654998953e2a251dd58");
-		console.log(balance);
-		reply.send(balance);
-	} catch (error) {
-		console.error("Error checking balance:", error);
-	}
-});
-
 fastify.register(tokenRoutes, { prefix: "/tokens" });
 fastify.register(pricesRoutes, { prefix: "/prices" });
 
