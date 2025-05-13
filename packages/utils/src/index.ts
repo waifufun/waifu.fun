2c18272ec8fe8e8e7a78111bfabdaeb30bacff9b
Joey <joey.kheireddine@outlook.com>
2025-05-13 22:24:38 +0200

    upd: moved function to SolanaRPCProvider as discussed during call

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -17,6 +17,9 @@ import DB from "@autofun/database";
 import moment from "moment";
 import redis from "@autofun/redis";
 import { SolanaRpcProvider } from "@autofun/rpc";
+import { EVMRpcProvider } from "@autofun/rpc";
+import { PublicKey } from "@solana/web3.js";
+import { getAddress } from "viem";
 
 dotenv.config();
 
@@ -277,3 +280,49 @@ export const updateCryptoPrices = async ({ cacheKey = "prices" }: { cacheKey?: s
 
 	return resolvedPrices;
 };
+
+export async function getTokenBalance({
+	chain,
+	wallet,
+	mintAddress,
+	chainId,
+}: {
+	chain: "solana" | "evm";
+	wallet: string;
+	mintAddress: string;
+	chainId: SolanaNetworkIds | EvmChainIds;
+}): Promise<number> {
+	if (chain === "evm") {
+		const provider = new EVMRpcProvider(chainId as EvmChainIds);
+		const checksummedAddress = getAddress(mintAddress);
+		const [balanceRaw, decimals] = await Promise.all([
+			provider.readErc20Contract(checksummedAddress, "balanceOf", [getAddress(wallet)]),
+			provider.readErc20Contract(checksummedAddress, "decimals", []),
+		]);
+		return Number(balanceRaw) / 10 ** Number(decimals);
+	}
+
+	if (chain === "solana") {
+		const provider = new SolanaRpcProvider(chainId as SolanaNetworkIds);
+		const mint = new PublicKey(mintAddress);
+		const owner = new PublicKey(wallet);
+
+		const tokenAccounts = await provider.connection.getTokenAccountsByOwner(owner, {
+			mint,
+		});
+
+		if (!tokenAccounts.value.length) return 0;
+
+		const accountData = tokenAccounts.value[0]?.account.data;
+		const amount = Number(accountData?.readBigUInt64LE(64));
+
+		const decimals = await provider.connection.getParsedAccountInfo(mint).then((info) => {
+			const data = (info.value?.data as any)?.parsed?.info;
+			return data?.decimals ?? 6;
+		});
+
+		return amount / 10 ** decimals;
+	}
+
+	throw new Error("Unsupported chain");
+}
