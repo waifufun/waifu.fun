602668d000fa74ec9b2b4fc155e5c229b9da7771
Joey <joey.kheireddine@outlook.com>
2025-05-14 12:07:16 +0200

    feat: added initial empty chat routes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,33 +4,35 @@ import helmet from "@fastify/helmet";
 import cors from "@fastify/cors";
 import tokenRoutes from "./routers/tokens";
 import pricesRoutes from "./routers/prices";
+import chatRoutes from "./routers/chat";
 
 const fastify = Fastify({
-	loggerInstance: logger,
+  loggerInstance: logger,
 });
 
 fastify.register(helmet);
 
 fastify.register(cors, {
-	allowedHeaders: ["*"],
-	origin: "*",
+  allowedHeaders: ["*"],
+  origin: "*",
 });
 
 fastify.get("/", (_, reply) => {
-	reply.send({ hello: "world" });
+  reply.send({ hello: "world" });
 });
 
 fastify.register(tokenRoutes, { prefix: "/tokens" });
 fastify.register(pricesRoutes, { prefix: "/prices" });
+fastify.register(chatRoutes, { prefix: "/chat" });
 
 const port = 3001;
 
 const start = async () => {
-	try {
-		await fastify.listen({ port });
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
+  try {
+    await fastify.listen({ port });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 };
 start();
