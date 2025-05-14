b44e074376a53e84355bced62042b91f9a604262
Joey <joey.kheireddine@outlook.com>
2025-05-14 12:24:41 +0200

    feat: added route that gives back the last 100 messages in a chat room

--- apps/backend/src/routers/chat.ts
+++ apps/backend/src/routers/chat.ts
@@ -1,9 +1,19 @@
 import type { FastifyInstance } from "fastify";
+import DB from "@autofun/database";
+import type { AddressLike, TChatRooms } from "@autofun/types";
 
 export default async function chatRoutes(fastify: FastifyInstance) {
-	/** Retrieve crypto token prices */
 	fastify.post("/history", async (request) => {
-		return { status: "OK" };
+		const body = request.body as { room: TChatRooms; contractAddress: AddressLike };
+
+		if (!body?.room || !body?.contractAddress) throw new Error("Missing contractAddress or room");
+
+		const messages = await DB.ChatMessage.find({
+			room: body.room,
+			contractAddress: body.contractAddress,
+		}).sort("-createdAt").limit(100).lean();
+
+		return messages;
 	});
 
 	fastify.post("/message", async (request) => {
