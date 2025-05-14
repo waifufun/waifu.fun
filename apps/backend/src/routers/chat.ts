2ac0f224189fa3e2da71433dac11aad16101ea73
Joey <joey.kheireddine@outlook.com>
2025-05-14 12:28:57 +0200

    feat: created initial route to send messages, no checks for auth being done yet

--- apps/backend/src/routers/chat.ts
+++ apps/backend/src/routers/chat.ts
@@ -3,6 +3,7 @@ import DB from "@autofun/database";
 import type { AddressLike, TChatRooms } from "@autofun/types";
 
 export default async function chatRoutes(fastify: FastifyInstance) {
+	// TODO - Add authentication middleware
 	fastify.post("/history", async (request) => {
 		const body = request.body as { room: TChatRooms; contractAddress: AddressLike };
 
@@ -11,12 +12,29 @@ export default async function chatRoutes(fastify: FastifyInstance) {
 		const messages = await DB.ChatMessage.find({
 			room: body.room,
 			contractAddress: body.contractAddress,
-		}).sort("-createdAt").limit(100).lean();
+		})
+			.sort("-createdAt")
+			.limit(100)
+			.lean();
 
 		return messages;
 	});
 
 	fastify.post("/message", async (request) => {
+		const body = request.body as { message: string; room: TChatRooms; contractAddress: AddressLike };
+
+		if (!body?.message || !body?.room || !body?.contractAddress) {
+			throw new Error("Missing one of the following parameters: message, room, contractAddress");
+		}
+
+		await DB.ChatMessage.create([
+			{
+				contractAddress: body.contractAddress,
+				message: body.message,
+				room: body?.room,
+			},
+		]);
+
 		return { status: "OK" };
 	});
 }
