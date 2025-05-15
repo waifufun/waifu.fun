f5e8655cd698a7c8e1202e4d1a824b58c19429f2
SuchJitter <jettenmostafa2@gmail.com>
2025-05-15 23:04:40 +0200

    feat: add env to enable uploading to datadog

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,27 +1,36 @@
 import winston from 'winston';
 const LOG_FILE_PATH = './logs/autofun-backend.log';
 
+const dogEnabled = process.env.DOG_LOGGER_ENABLED === 'true';
+
 const DogLogger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
-	winston.format.timestamp(),
-	winston.format.json()
+    winston.format.timestamp(),
+    winston.format.json()
   ),
   transports: [
-	new winston.transports.File({ filename: LOG_FILE_PATH }),
-	new winston.transports.Console({
-	  format: winston.format.combine(
-		winston.format.colorize(),
-		winston.format.printf(({ timestamp, level, message, ...meta }) => {
-		  let logMessage = `${timestamp} [${level}] ${message}`;
-		  if (Object.keys(meta).length) {
-			logMessage += ` ${JSON.stringify(meta, null, 2)}`;
-		  }
-		  return logMessage;
-		})
-	  )
-	})
+    new winston.transports.File({ filename: LOG_FILE_PATH }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.printf(({ timestamp, level, message, ...meta }) => {
+          let logMessage = `${timestamp} [${level}] ${message}`;
+          if (Object.keys(meta).length) {
+            logMessage += ` ${JSON.stringify(meta, null, 2)}`;
+          }
+          return logMessage;
+        })
+      )
+    })
   ],
 });
+
+if (!dogEnabled) {
+  const transportToRemove = DogLogger?.transports?.[0];
+  if (transportToRemove) {
+    DogLogger.remove(transportToRemove);
+  }
+}
 
 export default DogLogger;
