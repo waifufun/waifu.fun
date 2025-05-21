ee18218e1e685a1c28ec063a8956ebdf4a51a2e3
SuchJitter <jettenmostafa2@gmail.com>
2025-05-21 02:32:01 +0200

    upd: add more info

--- apps/frontend/src/components/hooks/providers/usePromptContext.tsx
+++ apps/frontend/src/components/hooks/providers/usePromptContext.tsx
@@ -4,15 +4,67 @@ import { createContext, useContext, useState, ReactNode } from "react";
 type PromptContextType = {
   prompt: string;
   setPrompt: (prompt: string) => void;
+  previousImages: string[];
+  mainImage: string;
+  name: string;
+  setName: (name: string) => void;
+  description: string;
+  setDescription: (description: string) => void;
+  ticker: string;
+  setTicker: (ticker: string) => void;
+  address: string;
+  buyAmount: number;
+  setBuyAmount: (amount: number) => void;
 };
 
 const PromptContext = createContext<PromptContextType | undefined>(undefined);
 
 export const PromptProvider = ({ children }: { children: ReactNode }) => {
   const [prompt, setPrompt] = useState("");
+  const [previousImages, setPreviousImages] = useState<string[]>([]);
+  const [mainImage, setMainImage] = useState<string>("/create/test-img.png");
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [ticker, _setTicker] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [buyAmount, setBuyAmount] = useState<number>(0);
+
+
+  const setTicker = (ticker: string) => {
+    if (ticker.length > 5) {
+      return;
+    }
+    // Allow empty string or a string that matches the regex
+    if (ticker !== "" && !/^[a-zA-Z0-9]+$/.test(ticker)) {
+      return;
+    }
+
+    _setTicker(ticker);
+  }
+
+  const props = {
+    prompt,
+    setPrompt,
+    previousImages,
+    setPreviousImages,
+    mainImage,
+    setMainImage,
+    name,
+    setName,
+    description,
+    setDescription,
+    ticker,
+    setTicker,
+    address,
+    setAddress,
+    buyAmount,
+    setBuyAmount,
+  } as PromptContextType;
+
+
 
   return (
-    <PromptContext.Provider value={{ prompt, setPrompt }}>
+    <PromptContext.Provider value={props}>
       {children}
     </PromptContext.Provider>
   );
