758fec7d3ee9244535b387921656bf59560eccb1
Joey <joey.kheireddine@outlook.com>
2025-05-22 19:35:24 +0200

    feat: added correct icons to tabs

--- apps/frontend/src/components/token-page/token-tabs.tsx
+++ apps/frontend/src/components/token-page/token-tabs.tsx
@@ -5,16 +5,27 @@ import AICreate from "./ai-create";
 import Chat from "./chat";
 import Agents from "./agents";
 import type { IToken } from "@autofun/types";
+import { ChartCandlestick, MessagesSquare, Stars, User, Users } from "lucide-react";
 
 export default function TokenTabs({ token }: { token: IToken }) {
 	return (
 		<Tabs defaultValue="trades">
 			<TabsList className="grid w-full grid-cols-5">
-				<TabsTrigger value="trades">Trades</TabsTrigger>
-				<TabsTrigger value="holders">Holders</TabsTrigger>
-				<TabsTrigger value="ai-create">AI Create</TabsTrigger>
-				<TabsTrigger value="chat">Chat</TabsTrigger>
-				<TabsTrigger value="agents">Agents</TabsTrigger>
+				<TabsTrigger value="trades" className="inline-flex gap-2">
+					Trades <ChartCandlestick size={24} />
+				</TabsTrigger>
+				<TabsTrigger value="holders" className="inline-flex gap-2">
+					Holders <Users size={24} />
+				</TabsTrigger>
+				<TabsTrigger value="ai-create" className="inline-flex gap-2">
+					AI Create <Stars size={24} />
+				</TabsTrigger>
+				<TabsTrigger value="chat" className="inline-flex gap-2">
+					Chat <MessagesSquare size={24} />
+				</TabsTrigger>
+				<TabsTrigger value="agents" className="inline-flex gap-2">
+					Agents <User size={24} />
+				</TabsTrigger>
 			</TabsList>
 			<TabsContent value="trades">
 				<Trades token={token} />
