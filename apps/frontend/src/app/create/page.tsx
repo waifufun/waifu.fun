01ea4493039860ae03aee688388c45d57bc04004
SuchJitter <jettenmostafa2@gmail.com>
2025-05-22 05:22:10 +0200

    upd: responsiveness

--- apps/frontend/src/app/create/page.tsx
+++ apps/frontend/src/app/create/page.tsx
@@ -3,50 +3,8 @@ import TokenTypeSelector from "@/components/create-token-page/token-type-selecto
 import { PromptProvider } from "@/components/hooks/providers/usePromptContext";
 import { usePrompt } from "@/components/hooks/providers/usePromptContext";
 import { useRef, useState } from "react";
-
-const PromptComponent = () => {
-	const { prompt, setPrompt } = usePrompt();
-	const textareaRef = useRef(null as any);
-	const [rows, setRows] = useState(3);
-	const MAX_ROWS = 5;
-
-	return (
-		<div
-			className="flex gap-4 bg-[#3333331A] px-4 items-center rounded-lg"
-			style={{
-				background: "linear-gradient(180deg, #171717 0%, #141414 100%)",
-			}}
-		>
-			<textarea
-				ref={textareaRef}
-				className="w-full rounded-lg py-3 resize-none overflow-aut focus:outline-none"
-				placeholder="Enter your prompt here..."
-				value={prompt}
-				onChange={(e) => setPrompt(e.target.value)}
-				rows={rows}
-				style={{
-					minHeight: "4.5rem",
-				}}
-			/>
-			<button
-				style={{
-					background: "linear-gradient(106.96deg, #141414 -24.65%, #131313 48.9%, #121212 109.26%)",
-				}}
-				className="border border-[#03FF24] rounded-lg hover:cursor-pointer px-4 py-2"
-			>
-				<p>Create</p>
-			</button>
-		</div>
-	);
-import TokenTypeSelector from "@/components/create-token-page/token-type-selector"
-import { PromptProvider } from "@/components/hooks/providers/usePromptContext"
-import { usePrompt } from "@/components/hooks/providers/usePromptContext"
-import { useEffect, useRef, useState } from "react";
-import { Info, Wallet } from "lucide-react";
-import next from "next";
 import Image from "next/image";
-
-
+import { Info, Wallet } from "lucide-react";
 
 const PromptComponent = () => {
     const { prompt, setPrompt, generateToken } = usePrompt();
@@ -115,52 +73,6 @@ const AiImageLoading = () => {
     )
 }
 
-const GeneratedImages = () => {
-	const { previousImages, mainImage } = usePrompt();
-
-	return (
-		<div className="w-full">
-			<div>
-				<img src={mainImage} alt="Main Image" className="w-full h-auto rounded-lg" />
-			</div>
-			<div>
-				<div className="grid grid-cols-3 gap-4 mt-4">
-					{previousImages.map((image, index) => (
-						<img key={index} src={image} alt={`Generated Image ${index + 1}`} className="w-full h-auto rounded-lg" />
-					))}
-				</div>
-			</div>
-		</div>
-	);
-};
-
-const TokenInfoInput = ({
-	title,
-	label,
-	value,
-	setValue,
-}: { title: string; label?: string; value: string; setValue: (value: string) => void }) => {
-	return (
-		<div className="flex flex-col gap-2 w-full">
-			<p className="text-xl font-[500]">{title}</p>
-			<div
-				className="flex items-center w-full px-4 gap-4"
-				style={{
-					background: "linear-gradient(180deg, #171717 0%, #141414 100%)",
-				}}
-			>
-				{label && <p className="text-[#8C8C8C] text-xl font-[500]">{label}</p>}
-				<input
-					className="w-full rounded-lg py-3 focus:outline-none"
-					type="text"
-					value={value}
-					onChange={(e) => setValue(e.target.value)}
-				/>
-			</div>
-		</div>
-	);
-};
-
 const TokenInfo = () => {
 	const { name, setName, description, setDescription, ticker, setTicker } = usePrompt();
 
@@ -177,63 +89,13 @@ const TokenInfo = () => {
 				<TokenInfoInput title="Description" value={description} setValue={setDescription} />
 			</div>
 			<GenerateAddress />
+            <BuyCoin/>
+            <ChoosePool/>
 		</div>
 	);
 };
 
-const GenerateAddress = () => {
-	const [suffix, setSuffix] = useState<string>("FUN");
-
-	return (
-		<div className="mt-4">
-			<div className="inline-block">
-				<p className="text-[#FFFFFF] font-[700] text-lg border-b border-b-[#03FF24]">GENERATE CUSTOM ADDRESS</p>
-				<div className="flex gap-4 mt-4">
-					<input
-						className="rounded-lg py-3 focus:outline-none text-center text-lg w-30"
-						type="text"
-						value={suffix}
-						onChange={(e) => setSuffix(e.target.value)}
-						placeholder="FUN"
-						style={{
-							background: "linear-gradient(106.96deg, #141414 -24.65%, #131313 48.9%, #121212 109.26%)",
-						}}
-					/>
-					<button className="border border-[#03FF24] rounded-lg hover:cursor-pointer px-4 py-2 w-full">
-						{" "}
-						{/* Use flex-1 to share width */}
-						<p>GENERATE</p>
-					</button>
-				</div>
-			</div>
-			<p className="text-[#03FF24] text-xl py-4">12FmM4TY8S9Qk3wCY1Ri5g1dUaGrAC5vfAVmiwdQmFUN</p>
-		</div>
-	);
-};
-
-export default function CreateTokenPage() {
-	return (
-		<PromptProvider>
-			<div className="flex justify-center px-40">
-				<div className="flex flex-col items-center mt-5 w-full">
-					<div>
-						<img src="/create/coin-machine.png" />
-					</div>
-					<div className="rounded-lg bg-[#3333331A] w-full overflow-hidden">
-						<TokenTypeSelector selected="auto" />
-						<div className="p-4">
-							<PromptComponent />
-							<div className="flex w-full gap-10 py-8">
-								<GeneratedImages />
-								<TokenInfo />
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</PromptProvider>
-	);
-}
+const GeneratedImages = () => {
     const {previousImages} = usePrompt();
     const {isGeneratingImage, changeMainImage} = usePrompt();
 
@@ -289,31 +151,6 @@ const TokenInfoInput = ({title, label, value, setValue } : {title: string, label
                     onChange={(e) => setValue(e.target.value)}
                 />
             </div>
-        </div>
-    )
-}
-
-const TokenInfo = () => {
-    const { name, setName, description, setDescription, ticker, setTicker } = usePrompt();
-
-
-
-    return (
-        <div className="w-full">
-            <p className="text-[#FFFFFF] font-[700] text-lg border-b border-b-[#03FF24] inline-block">COIN INFO</p>
-
-            <div className="flex flex-col gap-4 mt-2">
-                <div className="flex gap-8">
-                    <TokenInfoInput title="Name" value={name} setValue={setName}/>
-                    <TokenInfoInput title="Ticker" label="$" value={ticker} setValue={setTicker}/>
-                </div>
-
-                <TokenInfoInput title="Description" value={description} setValue={setDescription}/>
-            </div>
-            <GenerateAddress/>
-            <BuyCoin/>
-            <ChoosePool/>
-
         </div>
     )
 }
@@ -407,18 +244,18 @@ const ChoosePool = () => {
     const [pool, setPool] = useState("meteora");
 
     return (
-        <div className="flex justify-between w-full items-center">
-            <div className="flex gap-2 items-center">
-                <p className="text-xl font-[500]">Choose Pool</p>
-                <div className="flex px-2 py-1 rounded-lg gap-2"
+        <div className="flex flex-col gap-2 xl:flex-row xl:justify-between w-full xl:items-center">
+            <div className="flex flex-col xl:flex-row gap-2 items-center">
+                <p className="text-xl font-[500] whitespace-nowrap w-full xl:w-auto">Choose Pool</p>
+                <div className="flex px-2 py-1 rounded-lg gap-2 w-full"
                 style={{
-                    background: "linear-gradient(180deg, #171717 0%, #141414 100%)"
                 }}>
                     {poolData.map((poolIt) => (
                         <button key={poolIt.value} className="flex items-center gap-2 p-2 rounded-lg hover:cursor-pointer"
                         onClick={() => setPool(poolIt.value)}
                         style={{
                             border: poolIt.value === pool ? "1px solid #03FF24" : "1px solid #141414",
+                            background: "linear-gradient(180deg, #171717 0%, #141414 100%)"
                         }}>
                             <img src={poolIt.image} alt={poolIt.name} className="w-5 h-5"/>
                             <p className="text-[#8C8C8C] text-base font-[500]">{poolIt.name}</p>
@@ -437,13 +274,12 @@ const ChoosePool = () => {
     )
 }
 
-
 export default function CreateTokenPage() {
 
     return (
         <PromptProvider>
-            <div className="flex justify-center px-40">
-                <div className="flex flex-col items-center mt-5 w-full">
+            <div className="flex justify-center">
+                <div className="flex flex-col items-center mt-5 w-full max-w-[1100px]">
                     <div>
                         <img src="/create/coin-machine.png"/>
                     </div>
@@ -451,13 +287,14 @@ export default function CreateTokenPage() {
                         <TokenTypeSelector selected="auto"/>
                         <div className="p-4">
                             <PromptComponent/>
-                            <div className="flex w-full gap-10 py-8">
+                            <div className="flex flex-col lg:flex-row w-full gap-10 py-8">
                                 <GeneratedImages/>
                                 <TokenInfo/>
                             </div>
                         </div>
                     </div>
                 </div>
+                
             </div>
         </PromptProvider>
     )
--- apps/frontend/src/components/hooks/providers/usePromptContext.tsx
+++ apps/frontend/src/components/hooks/providers/usePromptContext.tsx
@@ -1,20 +1,4 @@
 "use client";
-import { createContext, useContext, useState, type ReactNode } from "react";
-
-type PromptContextType = {
-	prompt: string;
-	setPrompt: (prompt: string) => void;
-	previousImages: string[];
-	mainImage: string;
-	name: string;
-	setName: (name: string) => void;
-	description: string;
-	setDescription: (description: string) => void;
-	ticker: string;
-	setTicker: (ticker: string) => void;
-	address: string;
-	buyAmount: number;
-	setBuyAmount: (amount: number) => void;
 import { createContext, useContext, useState, useRef, useEffect, useCallback } from "react";
 import type { ReactNode } from "react";
 import { toast } from "sonner";
@@ -52,47 +36,6 @@ type PromptContextType = {
 const PromptContext = createContext<PromptContextType | undefined>(undefined);
 
 export const PromptProvider = ({ children }: { children: ReactNode }) => {
-	const [prompt, setPrompt] = useState("");
-	const [previousImages, setPreviousImages] = useState<string[]>([]);
-	const [mainImage, setMainImage] = useState<string>("/create/test-img.png");
-	const [name, setName] = useState<string>("");
-	const [description, setDescription] = useState<string>("");
-	const [ticker, _setTicker] = useState<string>("");
-	const [address, setAddress] = useState<string>("");
-	const [buyAmount, setBuyAmount] = useState<number>(0);
-
-	const setTicker = (ticker: string) => {
-		if (ticker.length > 5) {
-			return;
-		}
-		// Allow empty string or a string that matches the regex
-		if (ticker !== "" && !/^[a-zA-Z0-9]+$/.test(ticker)) {
-			return;
-		}
-
-		_setTicker(ticker);
-	};
-
-	const props = {
-		prompt,
-		setPrompt,
-		previousImages,
-		setPreviousImages,
-		mainImage,
-		setMainImage,
-		name,
-		setName,
-		description,
-		setDescription,
-		ticker,
-		setTicker,
-		address,
-		setAddress,
-		buyAmount,
-		setBuyAmount,
-	} as PromptContextType;
-
-	return <PromptContext.Provider value={props}>{children}</PromptContext.Provider>;
   const [prompt, setPrompt] = useState<string>("");
   const [name, setNameState] = useState<string>("");
   const [description, setDescriptionState] = useState<string>("");
