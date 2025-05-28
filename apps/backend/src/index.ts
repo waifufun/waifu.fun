0a29078ff9f995f73355b67005390cfdcfa8d9bc
Joey <joey.kheireddine@outlook.com>
2025-05-28 23:50:20 +0200

    feat: host 0.0.0.0

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -71,7 +71,7 @@ const port = 3001;
 
 const start = async () => {
 	try {
-		await fastify.listen({ port });
+		await fastify.listen({ port, host: "0.0.0.0" });
 	} catch (err) {
 		fastify.log.error(err);
 		process.exit(1);
