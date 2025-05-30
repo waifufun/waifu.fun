34e649dda10f59a38660ede31ce9c5bc2dfbb04d
Joey <joey.kheireddine@outlook.com>
2025-05-30 13:32:22 +0200

    upd: if linux use bucket

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -54,7 +54,7 @@ export default async function Home({
 					<GridListSelector />
 				</div>
 				{view === "grid" ? (
-					<div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
+					<div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4 w-full">
 						{tokens?.map((token: IToken) => (
 							<GridItem token={token} key={token.contractAddress} />
 						))}
