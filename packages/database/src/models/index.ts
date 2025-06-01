d820c7fd49933a23698a0b513d5fa8de15f67aaa
SuchJitter <jettenmostafa2@gmail.com>
2025-06-01 19:21:36 +0200

    changed file name to events-meta

--- packages/database/src/models/index.ts
+++ packages/database/src/models/index.ts
@@ -2,8 +2,8 @@ import Token from "./token";
 import User from "./user";
 import ChatMessage from "./chat-message";
 import Event from "./events";
-import EventsMeta from "./events_meta";
+import EventsMeta from "./events-meta";
 
 export { Token, User, ChatMessage, Event, EventsMeta };
 export type { IEvent } from "./events";
-export type { IEventsMeta } from "./events_meta";
+export type { IEventsMeta } from "./events-meta";
