0553659f50f237ac0a9c4bc2ceb9ce4cbe0df661
Joey <joey.kheireddine@outlook.com>
2025-06-02 17:03:36 +0200

    feat:  useIsClient from use-hooks to prevent SSR

--- apps/frontend/src/components/time-ago.tsx
+++ apps/frontend/src/components/time-ago.tsx
@@ -2,11 +2,13 @@
 
 import useTimeAgo from "@/hooks/use-timeago";
 import Skeleton from "./skeleton-loading";
+import { useIsClient } from "usehooks-ts";
 
 export default function TimeAgo({ date }: { date: number | Date | string }) {
 	const time = useTimeAgo({ date });
+	const client = useIsClient();
 
-	if (typeof window === "undefined") {
+	if (!client) {
 		return <Skeleton className="w-16" />;
 	}
 
