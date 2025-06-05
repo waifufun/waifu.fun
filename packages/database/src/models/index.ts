3b2dc00df31729fd10e8e9df6f16a4234c35d023
MarcoMandar <malicemandar@gmail.com>
2025-06-05 17:43:45 +0300

    fixes
    Signed-off-by: MarcoMandar <malicemandar@gmail.com>

--- packages/database/src/models/index.ts
+++ packages/database/src/models/index.ts
@@ -1,9 +1,10 @@
 import Token from "./token";
 import User from "./user";
-import ChatMessage from "./chat-message";=
+import ChatMessage from "./chat-message";
 import Agent from "./agent";
 import Event from "./events";
 import EventsMeta from "./events-meta";
+import Migration from "./migration";
 
 export { Token, User, ChatMessage, Event, EventsMeta, Agent, Migration };
 export type { IEvent } from "./events";
