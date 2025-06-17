a092c8d3ac584fe277b8656f8a9641673cd2c7e3
Joey <joey.kheireddine@outlook.com>
2025-06-17 19:27:28 +0200

    upd: updated switch

--- apps/frontend/src/app/token/[chain]/[chainId]/[contractAddress]/components/page-client.tsx
+++ apps/frontend/src/app/token/[chain]/[chainId]/[contractAddress]/components/page-client.tsx
@@ -170,7 +170,7 @@ export default function PageClient({
 											<Image
 												src={social.icon}
 												className={cn([
-													"size-6 select-none",
+													"size-4 select-none",
 													!social?.href ? "opacity-50 cursor-not-allowed" : "opacity-100 cursor-pointer",
 												])}
 												unoptimized
@@ -186,7 +186,7 @@ export default function PageClient({
 						<div>
 							<Image
 								src={token?.image}
-								className="float-right w-52 h-52 ml-4 mb-2 rounded-xl object-cover"
+								className="float-right w-52 h-52 ml-4 mb-2 rounded-none object-cover"
 								unoptimized
 								priority
 								width={208}
@@ -194,12 +194,12 @@ export default function PageClient({
 								alt="token"
 							/>
 							<div>
-								<span className="font-medium text-xl text-autofun-background-action-highlight uppercase block">
+								<span className="font-medium text-lg text-autofun-background-action-highlight uppercase block">
 									{token?.name}
 								</span>
-								<span className="font-medium text-xl uppercase block">{token?.ticker}</span>
+								<span className="font-medium text-lg uppercase block">{token?.ticker}</span>
 
-								<p className="text-base text-autofun-text-secondary">
+								<p className="text-xs text-autofun-text-secondary">
 									{token?.description
 										? token?.description
 										: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"}
