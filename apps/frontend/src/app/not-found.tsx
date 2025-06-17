297c25a23889bc37ac9ab67987fe70e430475565
Joey <joey.kheireddine@outlook.com>
2025-06-17 16:40:04 +0200

    upd: added suspense

--- apps/frontend/src/app/not-found.tsx
+++ apps/frontend/src/app/not-found.tsx
@@ -9,29 +9,29 @@ export default function NotFound() {
 	return (
 		<Suspense fallback={<div className="h-[80vh] flex items-center justify-center">Loading...</div>}>
 			<div className="h-[80vh] flex items-center justify-center">
-			<div className="my-auto flex flex-col items-center gap-6">
-				<Image src="/404.png" width={495} height={100} unoptimized priority alt="404" />
+				<div className="my-auto flex flex-col items-center gap-6">
+					<Image src="/404.png" width={495} height={100} unoptimized priority alt="404" />
 
-				<div className=" text-autofun-text-primary text-4xl font-bold font-satoshi uppercase tracking-[3.60px]">
-					Error
-				</div>
-
-				<div className="flex flex-col items-center gap-6">
-					<div className="flex flex-col items-center gap-3">
-						<div className="text-autofun-background-action-highlight text-2xl font-bold font-satoshi capitalize">
-							You auto-know… this page is gone.
-						</div>
-						<div className="text-autofun-text-primary text-lg font-bold font-satoshi capitalize">
-							Looks like you’ve reached a dead end. Navigate back to auto.fun and continue where the real action
-							happens.
-						</div>
+					<div className=" text-autofun-text-primary text-4xl font-bold font-satoshi uppercase tracking-[3.60px]">
+						Error
 					</div>
-					<Link href="/">
-						<Button variant="outline">Back to Trading</Button>
-					</Link>
+
+					<div className="flex flex-col items-center gap-6">
+						<div className="flex flex-col items-center gap-3">
+							<div className="text-autofun-background-action-highlight text-2xl font-bold font-satoshi capitalize">
+								You auto-know… this page is gone.
+							</div>
+							<div className="text-autofun-text-primary text-lg font-bold font-satoshi capitalize">
+								Looks like you’ve reached a dead end. Navigate back to auto.fun and continue where the real action
+								happens.
+							</div>
+						</div>
+						<Link href="/">
+							<Button variant="outline">Back to Trading</Button>
+						</Link>
+					</div>
 				</div>
 			</div>
-		</div>
 		</Suspense>
 	);
 }
--- apps/frontend/src/app/providers.tsx
+++ apps/frontend/src/app/providers.tsx
@@ -6,6 +6,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { SolanaProvider } from "@/providers/solana-provider";
 import { AnimationProvider } from "@/providers/animation-context";
+import { Suspense } from "react";
 
 const queryClient = new QueryClient();
 
@@ -13,27 +14,29 @@ const googleTagID = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID || "";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<TooltipProvider delayDuration={0}>
-			<ProgressProvider
-				height="4px"
-				color="#03FF24"
-				options={{
-					showSpinner: false,
-				}}
-				disableSameURL={false}
-				shallowRouting={true}
-				shouldCompareComplexProps
-			>
-				<QueryClientProvider client={queryClient}>
-					<AnimationProvider>
-						<SolanaProvider>
-							{children}
-							<Toaster />
-							<GoogleAnalytics gaId={googleTagID} />
-						</SolanaProvider>
-					</AnimationProvider>
-				</QueryClientProvider>
-			</ProgressProvider>
-		</TooltipProvider>
+		<Suspense>
+			<TooltipProvider delayDuration={0}>
+				<ProgressProvider
+					height="4px"
+					color="#03FF24"
+					options={{
+						showSpinner: false,
+					}}
+					disableSameURL={false}
+					shallowRouting={true}
+					shouldCompareComplexProps
+				>
+					<QueryClientProvider client={queryClient}>
+						<AnimationProvider>
+							<SolanaProvider>
+								{children}
+								<Toaster />
+								<GoogleAnalytics gaId={googleTagID} />
+							</SolanaProvider>
+						</AnimationProvider>
+					</QueryClientProvider>
+				</ProgressProvider>
+			</TooltipProvider>
+		</Suspense>
 	);
 }
