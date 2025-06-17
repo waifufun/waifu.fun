538b4d8bbf23fdecd19aa0fc91ae910f1d52d246
Joey <joey.kheireddine@outlook.com>
2025-06-17 11:06:38 +0200

    smol fixes

--- apps/backend/src/routers/user.ts
+++ apps/backend/src/routers/user.ts
@@ -2,32 +2,22 @@ import type { AddressLike, TURLLike } from "@autofun/types";
 import type { FastifyInstance } from "fastify";
 import DB from "@autofun/database";
 import { uploadBase64Image } from "@autofun/s3-uploader";
-import { randomUUID } from "node:crypto";
+
+import { getChecksummedAddress } from "@autofun/utils";
 
 export default async function userRoutes(fastify: FastifyInstance) {
-	// Get User by wallet address
-	fastify.post<{ Params: { address: AddressLike }; Body: { address: string } }>(
-		"/get-user",
-		async (request, reply) => {
-			const { address } = request.body;
+	fastify.post<{ Body: { address: AddressLike } }>("/get-user", async (request, reply) => {
+		const { address } = request.body;
 
-			try {
-				if (!address) throw new Error("No address was passed");
-				const user = await DB.User.findOne({ address });
+		if (!address) throw new Error("No address was passed");
+		const user = await DB.User.findOne({ address: getChecksummedAddress(address, "solana") }).lean();
+		if (!user) throw new Error("User not found");
+		return user;
+	});
 
-				if (!user) throw new Error("User not found");
-
-				return reply.send(user);
-			} catch (error) {
-				throw new Error(`Failed to find user: ${(error as Error).message}`);
-			}
-		},
-	);
-
-	// Change/Create Avatar on user
 	fastify.post<{
 		Body: {
-			address: string;
+			address: AddressLike;
 			image?: string;
 		};
 		Reply: { success: boolean; imageUrl?: string; error?: string };
@@ -38,28 +28,31 @@ export default async function userRoutes(fastify: FastifyInstance) {
 			throw new Error("Address is missing");
 		}
 
-		try {
-			const fileName = `${address}-${randomUUID()}`;
-			let uploadedUrl: TURLLike | false;
+		const checksummedAddress = getChecksummedAddress(address, "solana");
 
-			if (image) {
-				uploadedUrl = await uploadBase64Image(image, fileName, "avatar-images", 150, 150);
-			} else {
-				throw new Error("No image provided");
-			}
+		let uploadedUrl: TURLLike | false;
 
-			if (!uploadedUrl) {
-				throw new Error("Image upload failed");
-			}
-
-			await DB.User.findOneAndUpdate({ address }, { avatar: uploadedUrl });
-
-			return reply.send({ success: true });
-		} catch (error) {
-			return reply.code(500).send({
-				success: false,
-				error: error instanceof Error ? error.message : "Unknown error uploading profile image",
-			});
+		if (image) {
+			uploadedUrl = await uploadBase64Image(image, checksummedAddress, "avatar-images", 150, 150);
+		} else {
+			throw new Error("No image provided");
 		}
+
+		if (!uploadedUrl) {
+			throw new Error("Image upload failed");
+		}
+
+		await DB.User.updateOne(
+			{
+				address: checksummedAddress,
+			},
+			{
+				$set: {
+					avatar: uploadedUrl,
+				},
+			},
+		);
+
+		return { success: true };
 	});
 }
