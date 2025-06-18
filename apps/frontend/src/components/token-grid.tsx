6c55ec4c66bd1494b19090b4ef63d6791644d01b
Rayan Meziani <rayanmeziani95@outlook.com>
2025-06-18 14:18:21 +0200

    refactor: animation value change

--- apps/frontend/src/components/token-grid.tsx
+++ apps/frontend/src/components/token-grid.tsx
@@ -7,16 +7,15 @@ export default function TokenGrid({ tokens }: { tokens: IToken[] }) {
 	const columns = 5;
 
 	return (
-		<div className="columns-2 sm:columns-2 md:columns-3 lg:columns-5 gap-4 space-y-4">
+		<div className="columns-1 sm:columns-2 md:columns-3 lg:columns-5 gap-4 space-y-4">
 			{[...Array(columns)].map((_, colIndex) => {
 				const columnItems = tokens.filter((_, idx) => idx % columns === colIndex);
-				const getInitialY = () => (typeof window !== "undefined" && window.innerWidth < 640 ? 15 : -250);
 
 				return (
 					<motion.div
 						key={colIndex}
 						className="flex flex-col space-y-4"
-						initial={{ opacity: 0, y: getInitialY(), rotateX: 50 }}
+						initial={{ opacity: 0, y: -250, rotateX: 50 }}
 						animate={{ opacity: 1, y: 0, rotateX: 0 }}
 						transition={{
 							delay: colIndex * 0.35,
