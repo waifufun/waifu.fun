621527640f939f685eb3ad88ec4fed75a317420c
Rayan Meziani <rayanmeziani95@outlook.com>
2025-06-20 09:28:25 +0200

    revert changes

--- apps/frontend/src/components/token-grid.tsx
+++ apps/frontend/src/components/token-grid.tsx
@@ -2,80 +2,34 @@
 import { motion } from "framer-motion";
 import { GridItem } from "./grid-item";
 import type { IToken } from "@autofun/types";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { getTokens } from "@/lib/api";
-import { useEffect, useRef } from "react";
-import { LoaderCircle } from "lucide-react";
 
-export default function TokenGrid() {
+export default function TokenGrid({ tokens }: { tokens: IToken[] }) {
 	const columns = 5;
 	const columnKeys = Array.from({ length: columns }, (_, i) => `col${i + 1}`);
-	const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
-		queryKey: ["main-page-tokens"],
-		queryFn: async ({ pageParam = 1 }) => {
-			const res = await getTokens({ searchParams: { page: pageParam } });
-			return res as IToken[];
-		},
-		getNextPageParam: (lastPage, allPages) => {
-			return lastPage.length < 50 ? undefined : allPages.length + 1;
-		},
-		initialPageParam: 1,
-	});
-
-	const tokens: IToken[] = data?.pages.flat() ?? [];
-	const loaderRef = useRef<HTMLDivElement | null>(null);
-
-	useEffect(() => {
-		const observer = new IntersectionObserver(
-			(entries) => {
-				if (entries[0]?.isIntersecting && hasNextPage && !isFetchingNextPage) {
-					fetchNextPage();
-				}
-			},
-			{ threshold: 1.0 },
-		);
-
-		if (loaderRef.current) observer.observe(loaderRef.current);
-
-		return () => {
-			if (loaderRef.current) observer.unobserve(loaderRef.current);
-		};
-	}, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
 	return (
-		<div>
-			<div className="columns-1 sm:columns-2 md:columns-3 lg:columns-5 gap-4 space-y-4">
-				{columnKeys.map((colKey, colIndex) => {
-					const columnItems = tokens?.filter((_, idx) => idx % columns === colIndex);
-					console.log("column itmes ->", columnItems);
-					return (
-						<motion.div
-							key={colKey}
-							className="flex flex-col space-y-4"
-							initial={{ opacity: 0, y: -250, rotateX: 50 }}
-							animate={{ opacity: 1, y: 0, rotateX: 0 }}
-							transition={{
-								delay: colIndex * 0.35,
-								type: "spring",
-								stiffness: 25,
-								damping: 7,
-							}}
-						>
-							{columnItems?.map((token) => (
-								<GridItem key={token?.contractAddress} token={token} />
-							))}
-						</motion.div>
-					);
-				})}
-				<div ref={loaderRef} className="h-10 w-full" />
-			</div>
-			<div>
-				{isFetchingNextPage && (
-					<div className="w-full flex justify-center py-1 animate-spin text-muted-foreground">
-						<LoaderCircle className="h-10 w-10 text-[#03FF23]" />
-					</div>
-				)}
-			</div>
+		<div className="columns-1 sm:columns-2 md:columns-3 lg:columns-5 gap-4 space-y-4">
+			{columnKeys.map((colKey, colIndex) => {
+				const columnItems = tokens.filter((_, idx) => idx % columns === colIndex);
+				return (
+					<motion.div
+						key={colKey}
+						className="flex flex-col space-y-4"
+						initial={{ opacity: 0, y: -250, rotateX: 50 }}
+						animate={{ opacity: 1, y: 0, rotateX: 0 }}
+						transition={{
+							delay: colIndex * 0.35,
+							type: "spring",
+							stiffness: 25,
+							damping: 7,
+						}}
+					>
+						{columnItems.map((token) => (
+							<GridItem key={token.contractAddress} token={token} />
+						))}
+					</motion.div>
+				);
+			})}
 		</div>
 	);
 }
