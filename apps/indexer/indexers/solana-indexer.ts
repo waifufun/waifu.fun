7272ddb7bc7d1e7fe3c7bb521cbdd23160be9105
MarcoMandar <malicemandar@gmail.com>
2025-06-23 20:25:02 +0300

    remove comment
    Signed-off-by: MarcoMandar <malicemandar@gmail.com>

--- apps/indexer/indexers/solana-indexer.ts
+++ apps/indexer/indexers/solana-indexer.ts
@@ -18,7 +18,7 @@ export class SolanaIndexer extends BaseIndexer {
 		this.config = {
 			concurrencyLimit: 3,
 			maxSignatures: 500,
-			minBlock: process.env.NETWORK === "devnet" ? 388699253 : 322725834, // devnet
+			minBlock: process.env.NETWORK === "devnet" ? 388699253 : 322725834,
 			...config,
 		};
 
