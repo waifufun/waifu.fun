037f3ac263b7b71c3f99d7cd29087a6163cb3d18
Rayan Meziani <rayanmeziani95@outlook.com>
2025-06-26 14:45:04 +0200

    fix: fixed text going out of box when width gets smaller

--- apps/frontend/src/app/token/[chain]/[chainId]/[contractAddress]/components/page-client.tsx
+++ apps/frontend/src/app/token/[chain]/[chainId]/[contractAddress]/components/page-client.tsx
@@ -145,17 +145,18 @@ export default function PageClient({
 								height={208}
 								alt="token"
 							/>
-							<div className="flex flex-col">
-								<div className="flex items-center gap-2">
-									<span className="font-medium text-lg text-autofun-background-action-highlight uppercase block">
+							<div className="flex flex-col min-w-0">
+								<div className="flex items-center gap-2 min-w-0">
+									<span className="font-medium text-lg text-autofun-background-action-highlight uppercase block truncate">
 										{token?.name}
 									</span>
-									<span className="font-medium text-lg uppercase block">{token?.ticker}</span>
+									<span className="font-medium text-lg uppercase block truncate">{token?.ticker}</span>
 								</div>
-								<p className="text-xs text-autofun-text-secondary">
+
+								<p className="text-xs text-autofun-text-secondary break-words">
 									{token?.description
 										? token?.description
-										: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"}
+										: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem..."}
 								</p>
 							</div>
 						</div>
