82f70276f124bacc47371d51ee857fd7afc5eca3
Rayan Meziani <rayanmeziani95@outlook.com>
2025-06-27 10:01:09 +0200

    refactor: frontend you need auth to change avatar

--- apps/frontend/src/components/profile-page/avatar-image.tsx
+++ apps/frontend/src/components/profile-page/avatar-image.tsx
@@ -5,6 +5,7 @@ import { useRef, useState } from "react";
 import { Check } from "lucide-react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
+import useAddress from "@/hooks/use-address";
 
 export default function AvatarImage({ address, image }: { address: AddressLike; image: string }) {
 	const [imageFile, setImageFile] = useState<File | null>(null);
@@ -58,6 +59,9 @@ export default function AvatarImage({ address, image }: { address: AddressLike; 
 		},
 	});
 
+	const addressFromHook = useAddress();
+	console.log("use address hook ->", address);
+
 	return (
 		<div className="border-4 h-fit border-[#03FF24]/60 rounded-none shadow-[3px_3px_0px_rgba(3,255,36,0.4)] relative w-[150px]">
 			<Image
@@ -68,21 +72,25 @@ export default function AvatarImage({ address, image }: { address: AddressLike; 
 				unoptimized
 				className="object-cover"
 			/>
-			<input ref={fileInputRef} type="file" accept="image/*" onChange={handleFileChange} className="hidden" />
-			<div className="absolute px-2 w-full justify-between top-2 flex gap-2">
-				<button type="button" onClick={handleUploadClick} className="cursor-pointer bg-[#0C0C0C]/90 p-1 size-6">
-					<Image src="/profile/upload.svg" alt="Upload" width={24} height={24} />
-				</button>
-				{preview && preview !== image && (
-					<button
-						type="button"
-						onClick={() => imageFile && submitAvatarMutation.mutate(imageFile)}
-						className="bg-[#0C0C0C]/90 cursor-pointer p-1 w-6 h-6"
-					>
-						<Check className="text-[#03FF24]" width={14} height={14} />
-					</button>
-				)}
-			</div>
+			{addressFromHook === address && (
+				<>
+					<input ref={fileInputRef} type="file" accept="image/*" onChange={handleFileChange} className="hidden" />
+					<div className="absolute px-2 w-full justify-between top-2 flex gap-2">
+						<button type="button" onClick={handleUploadClick} className="cursor-pointer bg-[#0C0C0C]/90 p-1 size-6">
+							<Image src="/profile/upload.svg" alt="Upload" width={24} height={24} />
+						</button>
+						{preview && preview !== image && (
+							<button
+								type="button"
+								onClick={() => imageFile && submitAvatarMutation.mutate(imageFile)}
+								className="bg-[#0C0C0C]/90 cursor-pointer p-1 w-6 h-6"
+							>
+								<Check className="text-[#03FF24]" width={14} height={14} />
+							</button>
+						)}
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
