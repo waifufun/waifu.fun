8728beb052350276f0c5bfbe3861f15a882abf0c
Rayan Meziani <rayanmeziani95@outlook.com>
2025-07-01 20:22:21 +0200

    feature: ensure tokens bought are from autofun

--- apps/frontend/src/app/profile/[address]/components/page.tsx
+++ apps/frontend/src/app/profile/[address]/components/page.tsx
@@ -46,7 +46,7 @@ export default function Page({ balances }: { balances: { user: any; balances: an
 					data={{
 						username: user?.displayName,
 						address: user?.address || address,
-						tokensBought: tokensBought,
+						tokensBought: balances?.user?.tokensBoughtOnAutoFunAmount,
 						tokensCreated: tokensCreated?.length,
 						chains: [{ chain: "solana", chainId: 101, amount: 120 }],
 						points: user?.points,
