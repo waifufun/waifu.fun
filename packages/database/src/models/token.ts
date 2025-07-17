ac7a3e9264a9f310329c59972e1c5ae84e05f44d
Joey <joey.kheireddine@outlook.com>
2025-07-17 11:43:14 +0200

    upd: added missing indexes

--- packages/database/src/models/token.ts
+++ packages/database/src/models/token.ts
@@ -53,6 +53,8 @@ schema.plugin(paginate);
 schema.index({ contractAddress: 1, chain: 1, chainId: 1 }, { unique: true });
 schema.index({ hidden: 1, contractAddress: 1, chain: 1, chainId: 1 });
 schema.index({ hidden: 1, contractAddress: 1 });
+schema.index({ imported: 1, createdAt: -1, hidden: 1 });
+schema.index({ imported: 1, marketcap: -1, hidden: 1 });
 schema.index({ name: "text", ticker: "text", contractAddress: "text" });
 schema.index({ createdAt: -1 });
 const Model = Mongoose.model<IToken, PaginateModel<IToken>>("Token", schema);
