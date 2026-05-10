e595c9fc64f9e21adeb0b09ca4b9cae981e53eef
Sol <sol@shad0w.xyz>
2026-05-10 00:55:56 +0000

    refactor(contracts-evm): cleanup v3 interfaces and vault tests
    Co-authored-by: wakesync <shadow@shad0w.xyz>

--- packages/contracts-evm/contracts/TreasuryLP4.sol
+++ packages/contracts-evm/contracts/TreasuryLP4.sol
@@ -6,62 +6,7 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
-interface IFlapV2Pair {
-	function token0() external view returns (address);
-	function token1() external view returns (address);
-	function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
-	function price0CumulativeLast() external view returns (uint256);
-	function price1CumulativeLast() external view returns (uint256);
-}
-
-interface IFlapV2Router {
-	function WETH() external view returns (address);
-	function getAmountsOut(uint256 amountIn, address[] calldata path) external view returns (uint256[] memory amounts);
-	function swapExactETHForTokensSupportingFeeOnTransferTokens(
-		uint256 amountOutMin,
-		address[] calldata path,
-		address to,
-		uint256 deadline
-	) external payable;
-}
-
-interface IChainlinkFeed {
-	function latestRoundData()
-		external
-		view
-		returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
-
-interface IV4PoolManager {
-	struct PoolKey {
-		address currency0;
-		address currency1;
-		address hooks;
-		address poolManager;
-		uint24 fee;
-		int24 tickSpacing;
-	}
-
-	struct ModifyLiquidityParams {
-		int24 tickLower;
-		int24 tickUpper;
-		uint256 amount0Desired;
-		uint256 amount1Desired;
-		uint256 amount0Max;
-		uint256 amount1Max;
-		address recipient;
-		uint256 deadline;
-	}
-
-	function modifyLiquidity(PoolKey calldata key, ModifyLiquidityParams calldata params, bytes calldata hookData)
-		external
-		returns (uint256 positionId, uint128 liquidity, uint256 amount0, uint256 amount1);
-
-	function collect(uint256 positionId, address recipient) external returns (uint256 amount0, uint256 amount1);
-
-	function claimable(uint256 positionId) external view returns (uint256 amount0, uint256 amount1);
-}
+import {IChainlinkFeed, IFlapV2Pair, IFlapV2Router, IV4PoolManager} from "./interfaces/ITreasuryLPDeps.sol";
 
 contract TreasuryLP4 is Ownable, ReentrancyGuard {
 	using SafeERC20 for IERC20;
